dc8de8405ed2b69d36738fb693386bb953589b10
zaezero97 <xornjs1423@gmail.com>
2022-01-25 14:57:02 +0900

    LOLEasy Project 시작

---
+++ LOLEasy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LOLEasy
+//
+//  Created by 재영신 on 2022/01/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
